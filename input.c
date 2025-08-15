83a251e433671cfff8282eb37158d70b7cb5e714
flor4 <salanson.flora@outlook.fr>
2025-08-15 09:32:08 +0200

    Add: shell prompt display, input reading, and interactive mode detection function

---
+++ input.c
@@ -0,0 +1,48 @@
+#include "shell.h"
+
+/**
+ * print_prompt - Displays the shell prompt.
+ *
+ * Return: 0
+ */
+void print_prompt(void)
+{
+	printf(PROMPT);
+	fflush(stdout);
+}
+/**
+ * read_input - Reads a line from the standard input.
+ *
+ * Return: line
+ */
+char *read_input(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t n_chars;
+
+	n_chars = getline(&line, &len, stdin);
+
+	if (n_chars == -1)
+	{
+		free(line);
+		return (NULL);
+	}
+
+	if (line[n_chars - 1] == '\n')
+	{
+		line[n_chars - 1] = '\0';
+	}
+
+	return (line);
+}
+
+/**
+ * is_interactive - Checks if the shell is in interactive mode.
+ *
+ * Return: 1 if the shell is in interactive mode, 0 otherwise.
+ */
+int is_interactive(void)
+{
+	return (isatty(STDIN_FILENO));
+}
